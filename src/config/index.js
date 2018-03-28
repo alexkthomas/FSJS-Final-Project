93bc706e4078f43bc352929f8d12e8e014c7ac1d
alexkthomas <alexanderkeanethomas@gmail.com>
2018-03-27 23:00:19 -0400

    First commit

---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  appName: 'Our Glorious Node Project',
+  port: 3030,
+  db: {
+    username: 'test_user',
+    password: 'test_user',
+    host: 'ds159507.mlab.com:59507',
+    dbName: 'fsjs-class-project',
+  }
+
+}
