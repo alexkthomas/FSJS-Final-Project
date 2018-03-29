b40ab432d65abe0f961075f64d52f3afbd5f8a13
alexkthomas <alexanderkeanethomas@gmail.com>
2018-03-29 18:21:02 -0400

    awesome commit message

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,12 @@
 module.exports = {
-  appName: 'Our Glorious Node Project',
+  appName: 'Calorieapp',
   port: 3030,
   db: {
-    username: 'test_user',
-    password: 'test_user',
-    host: 'ds159507.mlab.com:59507',
+    host: 'ds147228.mlab.com:47228',
     dbName: 'fsjs-class-project',
+    username: 'alexkthomas',
+    password: 'kirstyann',
+
   }
 
 }
